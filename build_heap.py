aeb1f340ae6779058132b2fd3ad7530da07e5159
221RDB0088 <125836024+221RDB0088@users.noreply.github.com>
2023-03-20 00:00:56 +0000

    dd

--- build_heap.py
+++ build_heap.py
@@ -1,27 +1,26 @@
-def op(i, n, mas, swaps):
-    min_indek = i
-    l = 2*i + 1
-    if l < n and mas[l] < mas[min_indek]:
-        min_indek = l
-    r = 2*i + 2
-    if r < n and mas[r] < mas[min_indek]:
-        min_indek = r
-    if i != min_indek:
-        swaps.append((i, min_indek))
-        mas[i], mas[min_indek] = mas[min_indek], mas[i]
-        op(min_indek, n, mas, swaps)
+def main():
+    text = input("enter 'I' or  'F'")
+    if "F" in text:
+        file_name = input("file name: ")
+        if "a" not in file_name:
+            path = './tests/'+file_name
+            with open(path, 'r',encoding = 'utf-8') as file:
+                n = int(file.readline())
+                data = list(map(int,file.readline().split()))
 
-def into_heap(mas):
-    n = len(mas)
-    swaps = []
-    for i in range(n//2, -1, -1):
-        op(i, n, mas, swaps)
-    return swaps
+        elif "I" in text:
+            n = int(input())
+            data = list(map(int,input().split()))
 
-n = (input())
-mas = list(map(int, input().split()))
-swaps = into_heap(mas)
+        assert data is not None and len(data) == n
 
-print(len(swaps))
-for swap in swaps:
-    print(swap[0], swap[1])
+        swaps = build_heap(data)
+
+        assert len(swaps) <= n*4
+
+        print(len(swaps))
+        for i, j in swaps:
+            print(i,j)
+
+if __name__ == "__main__":
+    main()

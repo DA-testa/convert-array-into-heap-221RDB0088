2a04e9923329aaa2f655b2acb80222512dfaa92b
221RDB0088 <125836024+221RDB0088@users.noreply.github.com>
2023-03-20 00:26:10 +0000

    heap

--- build_heap.py
+++ build_heap.py
@@ -1,26 +1,83 @@
-def main():
-    text = input("enter 'I' or  'F'")
-    if "F" in text:
-        file_name = input("file name: ")
-        if "a" not in file_name:
-            path = './tests/'+file_name
-            with open(path, 'r',encoding = 'utf-8') as file:
-                n = int(file.readline())
-                data = list(map(int,file.readline().split()))
+def build_heap(data):
+    swaps = []
 
-        elif "I" in text:
-            n = int(input())
-            data = list(map(int,input().split()))
+    n = len(data)
+    # TODO: Creat heap and heap sort
 
-        assert data is not None and len(data) == n
+    for i in range(n // 2, -1, -1):
+        heap_sort(data, i, swaps)
+    return swaps
 
+
+def heap_sort(data, i, swaps):
+    
+    n = len(data)
+
+    parent = i
+    kreis =2*i+1
+
+    if kreis <= (n-1) and data[kreis] < data[parent]:
+        parent = kreis
+
+    labais =2*i+2  
+    if labais <= (n-1) and data[labais] < data[parent]:
+
+        parent = labais
+    if i != parent:
+        data[i], data[parent] = data[parent], data[i]
+        swaps.append((i, parent))
+        heap_sort(data, parent, swaps)
+
+
+
+
+def main():   
+    
+    # TODO : add input and corresponding checks
+    print("F vai I")
+    inp = input()
+    
+    #ja F
+    if "F" in inp :
+        fails =input()
+
+        if "a" not in fails:
+            path = './tests/' + fails
+            with open(path, 'r') as testfile:
+                n = int(testfile.readline().strip())
+                n1 = testfile.readline().strip()
+            data = list(map(int, n1.split()))
+            assert len(data) == n
+
+            swaps = build_heap(data)
+
+         # TODO: output how many swap
+
+
+            print(len(swaps))
+            for i, j in swaps:
+                print(i, j)
+
+   
+    # ja I
+    elif "I" in inp :
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+
+        #call for access
         swaps = build_heap(data)
 
-        assert len(swaps) <= n*4
+         # TODO: output how many swaps were made
 
         print(len(swaps))
         for i, j in swaps:
-            print(i,j)
+            print(i, j)
+    else:
+        print("error")
+        return
+
+
 
 if __name__ == "__main__":
     main()
